11d5a9ac0fdeae0bb5ba1938b270f16fc2d86eec
TrungKien1903 <trungkiennguyenduong@gmail.com>
2024-10-14 13:26:11 +0700

    .

--- WebGame/ProjectWebGame/views.py
+++ WebGame/ProjectWebGame/views.py
@@ -177,32 +177,6 @@ def create_game(request):
 #######################################
 
 @login_required
-def post_publish(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.publish()
-    return redirect('ProjectWebGame:post_detail', pk=pk)
-
-@login_required
-def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return redirect('ProjectWebGame:post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'ProjectWebGame/comment_form.html', {'form': form})
-
-@login_required
-def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('ProjectWebGame:post_detail', pk=comment.post.pk)
-
-@login_required
 def delete_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
     # Kiểm tra xem người dùng có quyền xóa không
